4727f054854ad7499f03fb3d0ea0e2a5f4e9fc59
Lisitius <90479443+Lisitius@users.noreply.github.com>
2022-04-17 11:03:50 +0200

    Init project, creation of home and about components and routes

---
+++ src/pages/About.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        sequi aliquid corrupti unde praesentium ratione modi quas sint vitae
+        fugiat vero incidunt necessitatibus, perferendis repellat, iure
+        voluptatem nostrum atque. Magnam sunt, ipsum neque facilis soluta fugiat
+        aliquam. Consequuntur fugit dolor minima fuga provident rerum ea. Enim
+        dolorem quasi necessitatibus similique velit atque cumque quaerat
+        explicabo illum voluptatum eveniet excepturi iste, tenetur labore
+        ducimus, omnis natus praesentium dolorum voluptatem. Sit, maiores
+        nesciunt aliquam ipsam aut maxime iure minus perferendis aspernatur
+        odio? Vero tempora in obcaecati neque esse nihil. Eos possimus rem,
+        dicta quos itaque at corrupti tenetur deleniti magnam tempore velit?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam itaque
+        modi explicabo hic nostrum est consequuntur qui aperiam natus eius
+        reiciendis mollitia iusto iste quo quaerat perspiciatis ullam iure
+        molestiae voluptatem, illum sunt doloribus non voluptates dicta. Quidem
+        cumque numquam inventore iusto ex, veritatis vitae laboriosam repellat
+        ipsa, rem necessitatibus molestiae quia placeat enim obcaecati
+        doloremque magnam quod, tempore deleniti cupiditate facere eos aliquam!
+        Blanditiis, enim minima. Fuga nobis magnam hic voluptas odio neque
+        doloremque blanditiis atque voluptate quasi iste doloribus vitae illo,
+        suscipit ducimus quae aut quis nisi. Inventore, illo vero! Explicabo
+        esse sapiente ea aperiam ab autem labore veritatis? Odit, similique
+        excepturi deserunt necessitatibus expedita voluptate nobis cupiditate
+        rerum facere saepe ab temporibus eveniet mollitia optio ducimus non
+        dolorem omnis ullam quidem, laboriosam laudantium sequi, iste quaerat.
+        Est quibusdam dolor ipsum? Nemo unde beatae incidunt necessitatibus
+        mollitia a nostrum fugit sapiente sint perspiciatis eaque impedit, porro
+        amet voluptate perferendis vel odit eum! Debitis repudiandae sit
+        architecto obcaecati voluptatem facilis corrupti perferendis autem quia
+        magni id sint, necessitatibus cumque quam! Sapiente, ab cum ea numquam
+        voluptatem modi aliquid quod quam natus? Ratione reprehenderit quidem
+        numquam ipsa iusto laboriosam blanditiis amet doloribus sit totam,
+        consequatur quod. Debitis aliquid cupiditate in?
+      </p>
+    </div>
+  );
+};
+
+export default About;
